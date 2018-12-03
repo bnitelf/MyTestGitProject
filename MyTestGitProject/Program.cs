e001bd67504c66727fe90a05519e5b2cc96ba604
Nattawut Wiginiyatanee <bnitelf@gmail.com>
2018-12-03 16:20:23 +0700

    Edit in program.cs file

--- MyTestGitProject/Program.cs
+++ MyTestGitProject/Program.cs
@@ -17,6 +17,8 @@ namespace MyTestGitProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // I Edit This
         }
     }
 }
